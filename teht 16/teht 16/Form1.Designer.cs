6b74f21092c1e67ac605f66b2f94201581980ff8
Rouhollah Tajik <rouhollah.tajik@edu.keuda.fi>
2024-02-05 02:24:41 +0200

    teht 16

---
+++ teht 16/teht 16/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace teht_16
+{
+    partial class CountDownForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MinuutitCB = new System.Windows.Forms.ComboBox();
+            this.SekunnitCB = new System.Windows.Forms.ComboBox();
+            this.AikaLB = new System.Windows.Forms.Label();
+            this.StartBT = new System.Windows.Forms.Button();
+            this.StopBT = new System.Windows.Forms.Button();
+            this.AjastinTM = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Minuutit:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sekunnit:";
+            // 
+            // MinuutitCB
+            // 
+            this.MinuutitCB.FormattingEnabled = true;
+            this.MinuutitCB.Location = new System.Drawing.Point(12, 50);
+            this.MinuutitCB.Name = "MinuutitCB";
+            this.MinuutitCB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MinuutitCB.Size = new System.Drawing.Size(128, 46);
+            this.MinuutitCB.TabIndex = 2;
+            // 
+            // SekunnitCB
+            // 
+            this.SekunnitCB.FormattingEnabled = true;
+            this.SekunnitCB.Location = new System.Drawing.Point(146, 50);
+            this.SekunnitCB.Name = "SekunnitCB";
+            this.SekunnitCB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SekunnitCB.Size = new System.Drawing.Size(131, 46);
+            this.SekunnitCB.TabIndex = 3;
+            // 
+            // AikaLB
+            // 
+            this.AikaLB.AutoSize = true;
+            this.AikaLB.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AikaLB.Location = new System.Drawing.Point(12, 99);
+            this.AikaLB.Name = "AikaLB";
+            this.AikaLB.Size = new System.Drawing.Size(283, 128);
+            this.AikaLB.TabIndex = 4;
+            this.AikaLB.Text = "00:00";
+            // 
+            // StartBT
+            // 
+            this.StartBT.Location = new System.Drawing.Point(12, 242);
+            this.StartBT.Name = "StartBT";
+            this.StartBT.Size = new System.Drawing.Size(112, 56);
+            this.StartBT.TabIndex = 5;
+            this.StartBT.Text = "Start";
+            this.StartBT.UseVisualStyleBackColor = true;
+            this.StartBT.Click += new System.EventHandler(this.StartBT_Click);
+            // 
+            // StopBT
+            // 
+            this.StopBT.Location = new System.Drawing.Point(146, 242);
+            this.StopBT.Name = "StopBT";
+            this.StopBT.Size = new System.Drawing.Size(112, 56);
+            this.StopBT.TabIndex = 6;
+            this.StopBT.Text = "Stop";
+            this.StopBT.UseVisualStyleBackColor = true;
+            this.StopBT.Click += new System.EventHandler(this.StopBT_Click);
+            // 
+            // AjastinTM
+            // 
+            this.AjastinTM.Interval = 1000;
+            this.AjastinTM.Tick += new System.EventHandler(this.AjastinTM_Tick);
+            // 
+            // CountDownForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 320);
+            this.Controls.Add(this.StopBT);
+            this.Controls.Add(this.StartBT);
+            this.Controls.Add(this.AikaLB);
+            this.Controls.Add(this.SekunnitCB);
+            this.Controls.Add(this.MinuutitCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CountDownForm";
+            this.Text = "Laskuri";
+            this.Load += new System.EventHandler(this.CountDownForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox MinuutitCB;
+        private ComboBox SekunnitCB;
+        private Label AikaLB;
+        private Button StartBT;
+        private Button StopBT;
+        private System.Windows.Forms.Timer AjastinTM;
+    }
+}
---
+++ teht 16/teht 16/Form1.cs
@@ -0,0 +1,58 @@
+namespace teht_16
+{
+    public partial class CountDownForm : Form
+    {
+        private int kokonaisaika;
+        public CountDownForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CountDownForm_Load(object sender, EventArgs e)
+        {
+            StopBT.Enabled = false;
+            for(int i = 0; i < 60; i++)
+            {
+                MinuutitCB.Items.Add(i.ToString());
+                SekunnitCB.Items.Add(i.ToString());
+            }
+            MinuutitCB.SelectedIndex = 0;
+            SekunnitCB.SelectedIndex = 10;
+        }
+
+        private void StartBT_Click(object sender, EventArgs e)
+        {
+            StartBT.Enabled= false;
+            StopBT.Enabled = true;
+            int minuutit = int.Parse(MinuutitCB.SelectedItem.ToString());
+            int sekunnit = int.Parse(SekunnitCB.SelectedItem.ToString());
+            kokonaisaika = (minuutit * 60) + sekunnit;
+            AjastinTM.Enabled = true;
+        }
+
+        private void StopBT_Click(object sender, EventArgs e)
+        {
+            StartBT.Enabled= true;
+            StopBT.Enabled = false;
+            kokonaisaika = 0;
+            AjastinTM.Enabled= false;
+            AikaLB.Text = "00:00";
+        }
+
+        private void AjastinTM_Tick(object sender, EventArgs e)
+        {
+            if (kokonaisaika > 0)
+            {
+                kokonaisaika--;
+                int minuutit = kokonaisaika / 60;
+                int sekuntit = kokonaisaika - (minuutit * 60);
+                AikaLB.Text = minuutit + ":" + sekuntit;
+            }
+            else
+            {
+                AjastinTM.Stop();
+                MessageBox.Show("Aikasi loppui!");
+            }
+        }
+    }
+}
