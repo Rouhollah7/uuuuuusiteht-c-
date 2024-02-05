f651afd6c417702ac8a3a52db97d81fe316d0fdf
Rouhollah Tajik <rouhollah.tajik@edu.keuda.fi>
2024-02-05 12:16:44 +0200

    teht 14 korjattu

--- teht 14/teht 14/Form1.cs
+++ teht 14/teht 14/Form1.cs
@@ -9,10 +9,10 @@ namespace teht_14
 
         private void talennaBT_Click(object sender, EventArgs e)
         {
-            string teksti = File.ReadAllText(@"C:\\Users\\rouhollahtajik\\source\\repos\\Windows-\\teht 14.text");
+            string teksti = File.ReadAllText(@"c:\\Users\\rouhollah.tajik\\source\\repos\\windowsForm\\teht 14\\paivakirja.txt");
             teksti += SyottoTB.Text;
             teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";
-            TextWriter text = new StreamWriter("C:\\Users\\rouhollahtajik\\source\\repos\\Windows-\\teht 14.text");
+            TextWriter text = new StreamWriter("c:\\Users\\rouhollah.tajik\\source\\repos\\windowsForm\\teht 14\\paivakirja.txt");
             text.Write(teksti);
             text.Close();
             Application.Exit();
